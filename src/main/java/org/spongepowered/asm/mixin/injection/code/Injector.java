129d9d40835035f15f705d859b967de11ee6ba4b
vfyjxf <42764413+vfyjxf@users.noreply.github.com>
2025-08-16 14:16:12 +0800

    fix java8 interface mixin

--- src/main/java/org/spongepowered/asm/mixin/injection/code/Injector.java
+++ src/main/java/org/spongepowered/asm/mixin/injection/code/Injector.java
@@ -447,7 +447,9 @@ public abstract class Injector {
     protected AbstractInsnNode invokeHandler(InsnList insns, MethodNode handler) {
         boolean isInterface = Bytecode.hasFlag(classNode, Opcodes.ACC_INTERFACE);
         boolean isPrivate = (handler.access & Opcodes.ACC_PRIVATE) != 0;
-        int invokeOpcode = this.isStatic ? Opcodes.INVOKESTATIC : isInterface ? Opcodes.INVOKEINTERFACE : isPrivate ? Opcodes.INVOKESPECIAL : Opcodes.INVOKEVIRTUAL;
+        int invokeOpcode = this.isStatic ? Opcodes.INVOKESTATIC :
+                           this.isInterface ? (isSynthetic && isPrivate ? Opcodes.INVOKESPECIAL : Opcodes.INVOKEINTERFACE) :
+                           isPrivate ? Opcodes.INVOKESPECIAL : Opcodes.INVOKEVIRTUAL;
         MethodInsnNode insn = new MethodInsnNode(invokeOpcode, this.classNode.name, handler.name, handler.desc, isInterface);
         insns.add(insn);
         this.info.addCallbackInvocation(handler);
