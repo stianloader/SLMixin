03b0312177d8ad1aae86fd5a398c1c2429db013c
vfyjxf_ <2331007009@qq.com>
2025-08-17 07:38:40 +0800

    update code from upstream correctly

--- src/main/java/org/spongepowered/asm/mixin/injection/code/Injector.java
+++ src/main/java/org/spongepowered/asm/mixin/injection/code/Injector.java
@@ -445,8 +445,8 @@ public abstract class Injector {
      * @return injected insn node
      */
     protected AbstractInsnNode invokeHandler(InsnList insns, MethodNode handler) {
-        boolean isInterface = Bytecode.hasFlag(classNode, Opcodes.ACC_INTERFACE);
-        boolean isPrivate = (handler.access & Opcodes.ACC_PRIVATE) != 0;
+        boolean isPrivate = Bytecode.hasFlag(handler, Opcodes.ACC_PRIVATE);
+        boolean isSynthetic = Bytecode.hasFlag(handler, Opcodes.ACC_SYNTHETIC);
         int invokeOpcode = this.isStatic ? Opcodes.INVOKESTATIC :
                            this.isInterface ? (isSynthetic && isPrivate ? Opcodes.INVOKESPECIAL : Opcodes.INVOKEINTERFACE) :
                            isPrivate ? Opcodes.INVOKESPECIAL : Opcodes.INVOKEVIRTUAL;
